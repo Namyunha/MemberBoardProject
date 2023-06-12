8633de3c39d228928048317f260ebcc3fddd69ab
Namyunha <ajs998@naver.com>
2023-06-13 08:27:56 +0900

    에러수정

--- src/main/java/com/example/memberboardproject/service/yhService/YhMemberService.java
+++ src/main/java/com/example/memberboardproject/service/yhService/YhMemberService.java
@@ -1,11 +1,11 @@
 package com.example.memberboardproject.service.yhService;
 
-import com.example.memberboardproject.repository.yhRepository.YhRepository;
+import com.example.memberboardproject.repository.yhRepository.YhMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class YhMemberService {
-    private final YhRepository yhRepository;
+//    private final YhMemberRepository yhMemberRepository;
 }
