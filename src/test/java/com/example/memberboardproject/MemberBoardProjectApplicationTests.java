36ac58147bc99d1afcb88f6d963cc40edcb03c23
Namyunha <ajs998@naver.com>
2023-06-12 14:30:17 +0900

    Initial commit

---
+++ src/test/java/com/example/memberboardproject/MemberBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memberboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberBoardProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
