752e70bd4ad03ef24290910d3cc2030d24b2c73f
guguping <qkfhandur@naver.com>
2023-06-13 20:23:27 +0900

    MemberSaveForm

--- src/main/java/com/example/memberboardproject/controller/hsController/HsMemberController.java
+++ src/main/java/com/example/memberboardproject/controller/hsController/HsMemberController.java
@@ -3,12 +3,12 @@ package com.example.memberboardproject.controller.hsController;
 import com.example.memberboardproject.dto.hsDto.HsMemberDTO;
 import com.example.memberboardproject.service.hsService.HsMemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -30,6 +30,14 @@ public class HsMemberController {
         hsMemberDTO.setMemberBirth(hsMemberDTO.getMemberYY() + "-" + hsMemberDTO.getMemberMM() + "-" + hsMemberDTO.getMemberDD());
         hsMemberService.save(hsMemberDTO);
         return "/HSPages/hsMemberPages/hsMemberLogin";
+    }
+    @PostMapping("/emailCheck")
+    public ResponseEntity emailCheck (@RequestBody String memberEmail) {
+        if (hsMemberService.findByMemberEmail(memberEmail)) {
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        } else {
+            return new ResponseEntity(HttpStatus.OK);
+        }
     }
 
 }
--- src/main/java/com/example/memberboardproject/repository/hsRepository/HsMemberRepository.java
+++ src/main/java/com/example/memberboardproject/repository/hsRepository/HsMemberRepository.java
@@ -4,6 +4,10 @@ import com.example.memberboardproject.entity.hsEntity.HsMemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HsMemberRepository extends JpaRepository<HsMemberEntity,Long> {
+
+    Optional<HsMemberEntity> findByMemberEmail(String memberEmail);
 }
