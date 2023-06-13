5a31425675fe3a703d99285107e4a474a1450c04
kwucslkm <kwucsani@naver.com>
2023-06-13 10:53:24 +0900

    km memberSave setting1 complete

---
+++ src/main/java/com/example/memberboardproject/repository/kmRepository/KmMemberFileRepository.java
@@ -0,0 +1,8 @@
+package com.example.memberboardproject.repository.kmRepository;
+
+import com.example.memberboardproject.entity.kmEntity.KmMemberFileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KmMemberFileRepository extends JpaRepository<KmMemberFileEntity, Long> {
+
+}
---
+++ src/main/java/com/example/memberboardproject/service/kmService/KmMemberService.java
@@ -0,0 +1,19 @@
+package com.example.memberboardproject.service.kmService;
+
+import com.example.memberboardproject.dto.kmdto.KmMemberDTO;
+import com.example.memberboardproject.entity.kmEntity.KmMemberEntity;
+import com.example.memberboardproject.repository.kmRepository.KmMemberFileRepository;
+import com.example.memberboardproject.repository.kmRepository.KmMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KmMemberService {
+    private final KmMemberRepository memberRepository;
+//    private final KmMemberFileRepository kmMemberFileRepository;
+    public void save(KmMemberDTO kmMemberDTO) {
+       KmMemberEntity kmMemberEntity= KmMemberEntity.savetoKmMemberEntity(kmMemberDTO);
+        memberRepository.save(kmMemberEntity);
+    }
+}
